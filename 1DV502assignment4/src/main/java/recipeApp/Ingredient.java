1e73a6321bd1b9f958e1011bcfb1cd8f06c6f7c3
Maja Hedegard <mh223pi@student.lnu.se>
2021-03-16 10:26:54 +0100

    add ingredient

--- 1DV502assignment4/src/main/java/recipeApp/Ingredient.java
+++ 1DV502assignment4/src/main/java/recipeApp/Ingredient.java
@@ -1,5 +1,50 @@
 package main.java.recipeApp;
 
 public class Ingredient {
-  
+
+  private String name;
+  private String unit;
+  private int price;
+  private int measure;
+
+   /**
+   * Creates a new ingredient.
+   */
+  public Ingredient(String name, String unit, int measure, int price) {
+    this.name = name;
+    this.unit = unit;
+    this.measure = measure;
+    this.price = price;
+  }
+
+  /**
+   * Returns the unit of the ingredient.
+   */
+  public String getunit() {
+    return unit;
+  }
+
+  /**
+   * Returns the measure of the ingredient.
+   */
+  public int getMeasure() {
+    return measure;
+  }
+
+  /**
+   * Returns the name of the ingredient.
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Returns the price of the ingredient.
+   */
+  public int getPrice() {
+    return price;
+  }
+
+
+
 }
