0a6a02f896e40b4f3952f8c6a3bca93a934082fd
Maja Hedegard <mh223pi@student.lnu.se>
2021-03-20 09:53:27 +0100

    add read and write to file

--- 1DV502assignment4/src/main/java/recipeApp/IngredientHandler.java
+++ 1DV502assignment4/src/main/java/recipeApp/IngredientHandler.java
@@ -1,6 +1,15 @@
 package main.java.recipeApp;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Map;
 import java.util.TreeMap;
+import java.util.Set;
 import java.util.Scanner;
 
 public class IngredientHandler {
@@ -22,7 +31,58 @@ public class IngredientHandler {
     Ingredient person = new Ingredient(name, unit, price);
     scan.close();
     return person;
-    
   }
 
+  private void addToTree() {
+    Ingredient ingredient = addIngredient();
+    ingredients.put(ingredient.getName(), ingredient);
+   }
+
+   public void readFromFile() {
+    try {
+      FileInputStream f = new FileInputStream(new File("lib//ingredients.txt"));
+      ObjectInputStream o = new ObjectInputStream(f);
+     
+      boolean isAvailable = true;
+      while(isAvailable){
+        if(f.available() != 0){
+         Ingredient ingredient = (Ingredient) o.readObject();    
+         ingredients.put(ingredient.getName(), ingredient);
+        }
+        else {
+        isAvailable = false;
+        }
+    }
+      o.close();
+      f.close();
+
+  } catch (FileNotFoundException e) {
+      System.out.println("File not found");
+  } catch (IOException e) {
+      e.printStackTrace();
+  } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+  }
+  }
+
+  public void writeToFile() {
+     
+    try {
+      FileOutputStream f = new FileOutputStream(new File("lib//ingredients.txt"));
+      ObjectOutputStream o = new ObjectOutputStream(f);
+      // Write objects to file
+      Set<Map.Entry<String, Ingredient> > entries 
+      = ingredients.entrySet(); 
+      for (Map.Entry<String, Ingredient> entry : entries) { 
+        o.writeObject(entry.getValue());
+    } 
+      o.close();
+      f.close();
+
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found");
+    } catch (IOException e) {
+      e.printStackTrace();
+    } 
+  }
 }
