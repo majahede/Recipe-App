bb96a23110729998839f0e6b07081deacbbb8f6a
Maja Hedegard <mh223pi@student.lnu.se>
2021-03-22 16:03:56 +0100

    add test

--- 1DV502assignment4/src/test/java/recipeApp/AppTest.java
+++ 1DV502assignment4/src/test/java/recipeApp/AppTest.java
@@ -1,10 +1,28 @@
 package test.java.recipeApp;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import main.java.recipeApp.Recipe;
+import main.java.recipeApp.RecipeIngredient;
+import static org.junit.Assert.*;
 
 public class AppTest {
   @Test 
-	public void testMax() {
+	public void testSetPrice() {
+    ArrayList<RecipeIngredient> ingredients = new ArrayList<RecipeIngredient>();
+    RecipeIngredient milk = new RecipeIngredient("milk", "unit", 2, "", 6, true);
+    RecipeIngredient sugar = new RecipeIngredient("milk", "tbsp", 2, "", 4, true);
+    RecipeIngredient cocoa = new RecipeIngredient("milk", "tbsp", 4, "", 5, true);
+    ingredients.add(milk);
+    ingredients.add(sugar);
+    ingredients.add(cocoa);
 
+    Recipe recipe = new Recipe("Hot chocolate", 2, ingredients, "mix ingredients" );
+   
+    assertEquals(40.0, recipe.setPrice(), 0);
+    ingredients.remove(sugar);
+    assertEquals(32.0, recipe.setPrice(), 0);
 	 } 
 }
